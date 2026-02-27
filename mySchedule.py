59c9660de639123693982e629b765fd693ffe6e1
Bchao2009 <145302497+Bchao2009@users.noreply.github.com>
2026-02-27 19:38:48 +0000

    This code prints my schedule for semester two with python code.

---
+++ mySchedule.py
@@ -0,0 +1,12 @@
+print("""
+Monday A        Monday B        Tuesday           Wednesday         Thursday          Friday
+-----------------------------------------------------------------------------------------------
+Dialogue II-B   English 10      Chemistry A       Modern World      Algebra II        Mandarin II
+Specials        Mandarin II     Algebra II        Dialogue II-B     English 10        Studio Art
+Chemistry A     World History   Comp Sci          Mandarin II       Research Prin     English 10
+Lunch           Lunch           Lunch             Lunch             Lunch             Lunch
+Algebra II      Research Prin   Studio Art        Algebra II        Chemistry A       World History
+                                Mandarin II       Chemistry A       Community Time    Community Time
+                                English 10                          Modern World       Comp Sci
+""")
+
