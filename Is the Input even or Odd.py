9aef2d76a3cffb028d9fc797168e366278478489
Bchao2009 <145302497+Bchao2009@users.noreply.github.com>
2026-03-06 19:55:04 +0000

    This code basically checks whether a number is even or odd.

---
+++ Is the Input even or Odd.py
@@ -0,0 +1,6 @@
+num = int(input("Enter a number: ")) #classifies the input by user as interger and number 
+
+if num % 2 == 0:
+    print("The number is even.") #identifies if number is even or odd + prints result
+else:
+    print("The number is odd.")
