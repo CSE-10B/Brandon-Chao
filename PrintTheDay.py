024c7454778e2bbfbb09ace79edb79eeaa2d5fd4
Bchao2009 <145302497+Bchao2009@users.noreply.github.com>
2026-03-06 20:03:27 +0000

    this code correlates a day of the week with a number provided by the user.

---
+++ PrintTheDay.py
@@ -0,0 +1,18 @@
+num = int(input("Enter a number (1-7): ")) #request user input from 1-7 
+
+if num == 1:
+    print("Monday")             #6 
+elif num == 2:
+    print("Tuesday")
+elif num == 3:
+    print("Wednesday")
+elif num == 4:
+    print("Thursday")
+elif num == 5:
+    print("Friday")
+elif num == 6:
+    print("Saturday")
+elif num == 7:
+    print("Sunday")
+else:     # if the number is not 1-7 is responds as invalid. 
+    print("Invalid number")
