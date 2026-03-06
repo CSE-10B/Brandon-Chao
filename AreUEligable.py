e56a2c7d493a074d3d3e142a7d04afab4ab50cde
Bchao2009 <145302497+Bchao2009@users.noreply.github.com>
2026-03-06 20:17:24 +0000

    fixed a bug

--- AreUEligable.py
+++ AreUEligable.py
@@ -1,7 +1,7 @@
 age = int(input("enter your age:"))         #provides input for user to put there age 
 if age < 0:
     print("Please provide a valid age")
-elif age >= 18:                             #categorizes code as old enough, not old enough, or invalid if its not an age.
+elif age >= 18:                             #categorizes code as old enough, not old enough, or if the input is not an age, invalid. 
     print("You are eligible to vote")
 else:
     print("You are not eligible to vote")
