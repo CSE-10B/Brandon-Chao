62b9a596621b1ed4f9a2768e3dedbb5f597eab5f
Bchao2009 <145302497+Bchao2009@users.noreply.github.com>
2026-03-10 00:03:04 +0000

    Flowchart that tells us if a triangle is equalateral or not.

--- EqualaterialTriangle.py
+++ EqualaterialTriangle.py
@@ -1,8 +1,8 @@
-side1 = float(input("Enter side 1: "))
+side1 = float(input("Enter side 1: "))          #these next three lines request user input for the three sides of the triangle.
 side2 = float(input("Enter side 2: "))
 side3 = float(input("Enter side 3: "))
 
 if side1 == side2 and side2 == side3:
-    print("The triangle is equilateral.")
+    print("The triangle is equilateral.")               #All three user inputs are anaylized and if they are all the same, it is an equilateral triangle.
 else:
-    print("The triangle is not equilateral.")
+    print("The triangle is not equilateral.")               #prints that the triangle is not equilateral if any of the sides are different.
