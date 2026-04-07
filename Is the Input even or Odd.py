d6d08fe666b0cb7019175a0da98120ad1d357633
Bchao2009 <145302497+Bchao2009@users.noreply.github.com>
2026-04-07 11:18:45 -0400

    Enhance code readability with comments
    Added comments to clarify code functionality.

--- Is the Input even or Odd.py
+++ Is the Input even or Odd.py
@@ -1,6 +1,9 @@
-num = int(input("Enter a number: ")) #classifies the input by user as interger and number 
-
+#asks the user to enter a number 
+num = int(input("Enter a number: ")) 
+# Use the modulus operator to divide the number by 2
+# If the remainder is 0, the number is even
 if num % 2 == 0:
-    print("The number is even.") #identifies if number is even or odd + prints result
+    print("The number is even.") 
+# If the remainder is not 0, the number is odd    
 else:
     print("The number is odd.")
